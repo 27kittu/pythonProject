765fcfd0881460975b0550b9ec0477cac1bfa42f
27kittu <cv27.one@gmail.com>
2021-07-11 01:09:49 +0530

    In this commit, there are few lines added for the user interface to enter the values including inputs, while loop and lists

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def inputs_bro(b_rate, S_rate, lots):
+from module1 import *
 
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
+trades = int(input('Please enter the number of trades: '))
+i = 1
+while i < trades + 1:
+    print('Please enter the trade details as buy rate, sell rate, lot size respectively')
+    brate = float(input("buy rate: "))
+    srate = float(input("sell rate: "))
+    lots = int(input("lot size: "))
+    lst_inputs = "lst_input"+str(i)
+    lst_inputs= [brate, srate, lots]
+    #bro_billing(lst_inputs)
+    i+=1
